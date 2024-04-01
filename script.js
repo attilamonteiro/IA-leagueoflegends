7502c79481a8d4dbde4b7556f35e65009fbadee2
attilamonteiro <atmaload@gmail.com>
2024-04-01 14:32:57 -0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const routes = {
-    champions: "http://sdw24.us-east-1.elasticbeanstalk.com/lol-api/champions",
-    ask: "http://sdw24.us-east-1.elasticbeanstalk.com/lol-api/champions/{id}/ask",
+    champions: "https://sdw24.us-east-1.elasticbeanstalk.com/lol-api/champions",
+    ask: "https://sdw24.us-east-1.elasticbeanstalk.com/lol-api/champions/{id}/ask",
   };
   
   const apiService = {
